207bf6f20cef4bd3f139539f35fb71aa7fd567b9
shuchityagi <shuchit91@gmail.com>
2018-02-24 12:47:54 +0530

    pending tx info api

--- src/server.js
+++ src/server.js
@@ -1,15 +1,73 @@
 var restify = require('restify');
-var config = require('./config.json');
+const bodyParser = require('body-parser');
+var config = require('../config.json');
+var mysql      = require('mysql');
+var unirest = require('unirest');
 
-function basicStats(req, res, next) {
+var connection = mysql.createConnection({
+  host     : config.db_host,
+  user     : config.db_user,
+  password : config.db_pass,
+  database : config.db_name
+});
+ 
 
+function getBlockInfo(req, res, next) {
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log('connected as id ' + connection.threadId);
+      });
+
+    connection.query('SELECT * FROM block_info ORDER BY id DESC LIMIT 1;', function (error, results, fields) {
+        if (error){
+            res.json({'status':'failed','message':'Error while fetching data'});
+            throw error;
+        }
+        else{
+            var result = [];
+            var xrk_supply = (config.mining_reward * results[0].best_block) + config.premined_quant;
+            result.push({'status':'success',
+                        'data':{'best_block':results[0].best_block,
+                        'xrk_supply':xrk_supply,
+                        'best_block_timestamp':results[0].block_time}
+                    });
+            res.json(result);
+        }
+      });
+    connection.end();  
+}
+
+function getPendingTx(req, res, next){
+    var auth = 'Basic ' + Buffer.from(config.rk_user + ':' + config.rk_pass).toString('base64');
+    var req = unirest("POST", config.rk_host+':'+config.rk_port);
+
+    req.headers({
+    "cache-control": "no-cache",
+    "authorization": auth,
+    "content-type": "application/json"
+    });
+
+    req.type("json");
+    req.send({
+    "method": "getmempoolinfo",
+    "id": 1,
+    "chain_name": config.rk_chain
+    });
+
+    req.end(function (response) {
+    if (response.error){
+        res.json({'status':'failed','message':'Error while fetching data'});
+        throw new Error(res.error);
+    } else res.json(response.body);
+    });
 }
 
 var server = restify.createServer();
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
-server.get('/stats/', basicStats);
-
+server.get('/blockInfo/', getBlockInfo);
+server.get('/pendingTx/', getPendingTx);
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
   });
