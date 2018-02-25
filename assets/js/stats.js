cb4caa5d48924d8f237111a964e0ed30813bb6af
shuchityagi <shuchit91@gmail.com>
2018-02-25 12:44:59 +0530

    api updates

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -11,27 +11,171 @@
 $(document).ready(function(){
 		 // Animate loader off screen
            $(".se-pre-con").fadeOut("slow");  // fadeout the preloader
-        
-           $.ajax({
-            url: 'http://test-stats.recordskeeper.co/api/blockInfo',
-            type: 'GET',
-            dataType: 'json',
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true,
-                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Content-Type':'application/json'
-            },
-            success: function (data, textStatus, xhr) {
-                console.log(data);
-            },
-            error: function (xhr, textStatus, errorThrown) {
-                console.log('Error in Operation',xhr);
-            }
-        });
+           getBlockInfo();
+           getPendingTx();
+           getAverageTime();
 
 });
+
+function getBlockInfo(){
+    $.ajax({
+        url: 'http://test-stats.recordskeeper.co/api/blockInfo',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Content-Type':'application/json'
+        },
+        success: function (data, textStatus, xhr) {
+            console.log(data);
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            console.log('Error in Operation',xhr);
+
+            var data = [
+                {
+                    "status": "success",
+                    "data": {
+                        "best_block": 20758,
+                        "xrk_supply": 5020758000000000,
+                        "best_block_timestamp": 1519480950
+                    }
+                }
+            ];
+            jQuery.parseJSON(JSON.stringify(data));
+            $("#best_block").html("#"+data[0].data.best_block);
+            $("#best_block_time").html(data[0].data.best_block_timestamp);
+            $("#xrk_supply").html(data[0].data.xrk_supply/100000000);
+        }
+    });
+}
+
+function getPendingTx(){
+    $.ajax({
+        url: 'http://test-stats.recordskeeper.co/api/pendingTx',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Content-Type':'application/json'
+        },
+        success: function (data, textStatus, xhr) {
+            console.log(data);
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            console.log('Error in Operation',xhr);
+            var data = {
+                "result": {
+                    "size": 0,
+                    "bytes": 0
+                },
+                "error": null,
+                "id": 1
+            };
+            jQuery.parseJSON(JSON.stringify(data));
+            $("#pending_tx").html("#"+data.result.size);
+        }
+    });
+}
+
+function getAverageTime(){
+    $.ajax({
+        url: 'http://test-stats.recordskeeper.co/api/avgTime',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Content-Type':'application/json'
+        },
+        success: function (data, textStatus, xhr) {
+            console.log(data);
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            console.log('Error in Operation',xhr);
+            var data = [{
+                "status": "success",
+                "data": {
+                    "avg_time": 35.05858310626703
+                }
+            }];
+            jQuery.parseJSON(JSON.stringify(data));
+            $("#avg_time").html("#"+data[0].data.avg_time);
+        }
+    });
+}
+
+function getChartData(){
+    $.ajax({
+        url: 'http://test-stats.recordskeeper.co/api/chart',
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Content-Type':'application/json'
+        },
+        success: function (data, textStatus, xhr) {
+            console.log(data);
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            console.log('Error in Operation',xhr);
+            var data = [{
+                "status": "success",
+                "data": {
+                    "results": [{
+                        "id": 35,
+                        "difficulty": 0.00781238,
+                        "hash_rate": 0.000223210857142857,
+                        "created_at": "2018-02-24T14:40:04.000Z"
+                    }, {
+                        "id": 34,
+                        "difficulty": 0.00781238,
+                        "hash_rate": 0.000223210857142857,
+                        "created_at": "2018-02-24T14:35:03.000Z"
+                    }, {
+                        "id": 33,
+                        "difficulty": 0.00781238,
+                        "hash_rate": 0.000223210857142857,
+                        "created_at": "2018-02-24T14:30:04.000Z"
+                    }, {
+                        "id": 32,
+                        "difficulty": 0.00781238,
+                        "hash_rate": 0.000223210857142857,
+                        "created_at": "2018-02-24T14:25:04.000Z"
+                    }, {
+                        "id": 31,
+                        "difficulty": 0.00781238,
+                        "hash_rate": 0.000223210857142857,
+                        "created_at": "2018-02-24T14:20:04.000Z"
+                    }, {
+                        "id": 30,
+                        "difficulty": 0.00781238,
+                        "hash_rate": 0.000223210857142857,
+                        "created_at": "2018-02-24T14:15:04.000Z"
+                    }, {
+                        "id": 29,
+                        "difficulty": 0.00781238,
+                        "hash_rate": 0.000223210857142857,
+                        "created_at": "2018-02-24T14:10:04.000Z"
+                    }]
+                }
+            }];
+            jQuery.parseJSON(JSON.stringify(data));
+            $("#avg_time").html("#"+data[0].data.avg_time);
+        }
+    });
+}
 
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
--- src/server.js
+++ src/server.js
@@ -40,6 +40,29 @@ function getBlockInfo(req, res, next) {
     connection.end();  
 }
 
+function getAvgTime(req, res, next) {
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log('connected as id ' + connection.threadId);
+      });
+
+    connection.query('SELECT AVG(time_diff) FROM block_info WHERE created_at > DATE_SUB(NOW(), INTERVAL 1 DAY) ORDER BY id ASC;', function (error, results, fields) {
+        if (error){
+            res.json({'status':'failed','message':'Error while fetching data'});
+            throw error;
+        }
+        else{
+            var result = [];
+            result.push({'status':'success',
+                        'data':{'avg_time':results[0]['AVG(time_diff)']}
+                    });
+            res.json(result);
+        }
+      });
+    connection.end();  
+}
+
 function getPendingTx(req, res, next){
     var auth = 'Basic ' + Buffer.from(config.rk_user + ':' + config.rk_pass).toString('base64');
     var req = unirest("POST", config.rk_host+':'+config.rk_port);
@@ -65,11 +88,61 @@ function getPendingTx(req, res, next){
     });
 }
 
+function getChartInfo(req, res, next) {
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log('connected as id ' + connection.threadId);
+      });
+
+    connection.query('SELECT * FROM chart_values ORDER BY id DESC LIMIT 7;', function (error, results, fields) {
+        if (error){
+            res.json({'status':'failed','message':'Error while fetching data'});
+            throw error;
+        }
+        else{
+            var result = [];
+            result.push({'status':'success',
+                        'data':{results}
+                    });
+            res.json(result);
+        }
+      });
+    connection.end();  
+}
+
+function gettxInfo(req, res, next) {
+    
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log('connected as id ' + connection.threadId);
+      });
+
+    connection.query('SELECT * FROM transaction_info ORDER BY id DESC LIMIT 1;', function (error, results, fields) {
+        if (error){
+            res.json({'status':'failed','message':'Error while fetching data'});
+            throw error;
+        }
+        else{
+            var result = [];
+            result.push({'status':'success',
+                        'data':{results}
+                    });
+            res.json(result);
+        }
+      });
+    connection.end();  
+}
+
+
+
 var server = restify.createServer();
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.get('/blockInfo/', getBlockInfo);
 server.get('/pendingTx/', getPendingTx);
+server.get('/avgTime/', getAvgTime);
+server.get('/chart/', getChartInfo);
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
   });
