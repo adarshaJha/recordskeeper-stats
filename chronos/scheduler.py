d5b9ff31b8f684a2a6781f312d7a59cf6d8e31af
Rohit Shukla <rohitshukla556@gmail.com>
2018-02-26 18:23:07 +0530

    updated file path

--- chronos/scheduler.py
+++ chronos/scheduler.py
@@ -1,8 +1,8 @@
 from crontab import CronTab
  
 my_cron = CronTab(user='enter_your_user_name')
-job = my_cron.new(command='python /home/user/stats/server.py')
-job2 = my_cron.new(command='python /home/user/stats/chart.py')
+job = my_cron.new(command='python file path')
+job2 = my_cron.new(command='python file path')
 job.minute.every(1)
 job2.hour.every(1)
 
