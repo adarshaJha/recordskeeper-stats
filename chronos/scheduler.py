3b4843ffc90ca30f021628b02059310bfcbae3d7
Rohit Shukla <rohitshukla556@gmail.com>
2018-02-26 17:45:57 +0530

    User details updated

--- chronos/scheduler.py
+++ chronos/scheduler.py
@@ -1,8 +1,8 @@
 from crontab import CronTab
  
-my_cron = CronTab(user='rohit')
-job = my_cron.new(command='python /home/rohit/stats/server.py')
-job2 = my_cron.new(command='python /home/rohit/stats/chart.py')
+my_cron = CronTab(user='enter_your_user_name')
+job = my_cron.new(command='python /home/user/stats/server.py')
+job2 = my_cron.new(command='python /home/user/stats/chart.py')
 job.minute.every(1)
 job2.hour.every(1)
 
