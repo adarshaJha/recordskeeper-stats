3d5b3b5577c72431c1c81aa9a1143bd8d8e78573
Rohit Shukla <rohitshukla556@gmail.com>
2018-04-24 23:59:01 +0530

    Update config-mainnet-sample.php

--- src/config-mainnet-sample.php
+++ src/config-mainnet-sample.php
@@ -10,8 +10,12 @@ return array(
    "rk_user"  => "rk user",
    "rk_pass"  => "rk pass",
    "rk_chain"  => "recordskeeper",
-   "premined_tokens"  => 0,
-   "mining_reward"  => 1000000000,
-   "refresh_rate" => 5,
+   "premined_tokens"  => 300000000,
+   "mining_reward"  => 10,
+   "dir" => '/home/user/.rk/recordskeeper',
+   "genesis-miner" => "1APSxFtBTbtXFacQ4Tn3bcPLHdqwN8SBx4fmEh",
+   "genesis-miner-tx-id" => "d75db359dc01b18a9ec1c2f13d506268e8abe3c0a15d1ac235a57b31352e072c",
+   "difficulty"=> 0.06249911,
+   "hash-rate" => 0.00416661,
    "genesis-timestamp" => 1519486418
 );
