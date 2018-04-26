52b2cfae17008216a9f46259d3e765769c9c96e8
Rohit Shukla <rohitshukla556@gmail.com>
2018-04-26 17:16:43 +0530

    Update config-mainnet-sample.php

--- src/config-mainnet-sample.php
+++ src/config-mainnet-sample.php
@@ -12,10 +12,11 @@ return array(
    "rk_chain"  => "recordskeeper",
    "premined_tokens"  => 300000000,
    "mining_reward"  => 10,
-   "dir" => '/home/user/.rk/recordskeeper',
+   "dir" => '/home/ubuntu/.rk/recordskeeper',
    "genesis-miner" => "1APSxFtBTbtXFacQ4Tn3bcPLHdqwN8SBx4fmEh",
    "genesis-miner-tx-id" => "d75db359dc01b18a9ec1c2f13d506268e8abe3c0a15d1ac235a57b31352e072c",
    "difficulty"=> 0.06249911,
    "hash-rate" => 0.00416661,
-   "genesis-timestamp" => 1519486418
+   "lock-file" => "mainnet-lock.txt",
+   "genesis-timestamp" => 1522837825
 );
