2e7de5de8b29672594cc4603ba28b142c1a91a76
Rohit Shukla <rohitshukla556@gmail.com>
2018-04-26 17:17:34 +0530

    Update config-testnet-sample.php

--- src/config-testnet-sample.php
+++ src/config-testnet-sample.php
@@ -17,5 +17,6 @@ return array(
    "genesis-miner-tx-id" => "d8853dc3a446e3117ba2932df0093ba6d740ec6b9d7193eb75bf0be60e3f3175",
    "difficulty"=> 0.00097655,
    "hash-rate" => 0.00006510,
+   "lock-file" => "testnet-lock.txt",
    "genesis-timestamp" => 1522831575
 );
