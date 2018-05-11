a3899c044a6f7779069eed1f8ab7d4462158acf9
rohitshukla11 <rohitshukla556@gmail.com>
2018-05-11 14:41:02 +0530

    db scripts added

---
+++ src/config-mainnet-sample.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+   "db_user"  => "user",
+   "db_name"  => "namec",
+   "db_host"  => "host",
+   "db_pass"  => "pass",
+   "rk_host"  => "rk host",
+   "rk_port"  => 0,
+   "rk_user"  => "rk user",
+   "rk_pass"  => "rk pass",
+   "rk_chain"  => "recordskeeper",
+   "premined_tokens"  => 300000000,
+   "mining_reward"  => 10,
+   "dir" => '/home/ubuntu/.rk/recordskeeper',
+   "genesis-miner" => "1APSxFtBTbtXFacQ4Tn3bcPLHdqwN8SBx4fmEh",
+   "genesis-miner-tx-id" => "d75db359dc01b18a9ec1c2f13d506268e8abe3c0a15d1ac235a57b31352e072c",
+   "difficulty"=> 0.06249911,
+   "hash-rate" => 0.00416661,
+   "lock-file" => "mainnet-lock.txt",
+   "access-key" => "ipstack_access_key",
+   "genesis-timestamp" => 1522837825
+);
